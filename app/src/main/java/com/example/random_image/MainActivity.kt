eaa760b32122d4060d04576d3a5296482179b3e3
munin <6310682601@student.tu.ac.th>
2023-04-19 00:09:51 +0700

    finish basic requirement

--- app/src/main/java/com/example/random_image/MainActivity.kt
+++ app/src/main/java/com/example/random_image/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.random_image
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,8 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,6 +35,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavController
 import com.example.random_image.ui.theme.Random_ImageTheme
+import coil.compose.rememberImagePainter
 
 enum class Screen {
     Home, Show
@@ -52,34 +51,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController,
-                        startDestination = com.example.random_image.Screen.Home.name
-                    ) {
-                        composable(com.example.random_image.Screen.Home.name) {
-                            HomeScreen(navController = navController)
-                        }
-                        composable("Show Picture") {
-                            ShowPic(name = "Show Picture", navController = navController)
-                        }
-                    }
+                    RandomImageScreen()
                 }
             }
         }
     }
 }
+var error: String = ""
+var url: String = ""
+var width: String = ""
+var height: String = ""
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
-    var width by remember { mutableStateOf("") }
-    var height by remember { mutableStateOf("") }
+fun RandomImageScreen(modifier: Modifier = Modifier) {
     Column(
         Modifier
             .fillMaxSize()
             .padding(0.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var width by remember { mutableStateOf("") }
+        var height by remember { mutableStateOf("") }
         TextField(
             value = width,
             onValueChange = { width = it },
@@ -89,9 +82,12 @@ fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
                     fontSize = 20.sp,
                 )
             },
-            modifier = Modifier.fillMaxWidth().padding(60.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(60.dp),
             singleLine = true
         )
+
         TextField(
             value = height,
             onValueChange = { height = it },
@@ -101,22 +97,271 @@ fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
                     fontSize = 20.sp,
                 )
             },
-            modifier = Modifier.fillMaxWidth().padding(60.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(60.dp),
             singleLine = true
         )
+        Text(
+            text = "Select your picture type.",
+            fontSize = 20.sp,
+            )
+        Spacer(modifier = Modifier.height(30.dp))
+        Row() {
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/movie"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }}) {
+                Text(
+                    text = "movie",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/game"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }}) {
+                Text(
+                    text = "game",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/album"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "album",
+                    fontSize = 20.sp,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row() {
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/book"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "book",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/face"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }}) {
+                Text(
+                    text = "face",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/fashion"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "fashion",
+                    fontSize = 20.sp,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row() {
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/shoes"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "shoes",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/watch"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "watch",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/furniture"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "furniture",
+                    fontSize = 20.sp,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row() {
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/dog"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "dog",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/paris"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }
+            }) {
+                Text(
+                    text = "paris",
+                    fontSize = 20.sp,
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            Button(onClick = {
+                val w = width.toIntOrNull()
+                val h = height.toIntOrNull()
+                if (w != null || h != null){
+                    if(w in 8..2000 && h in 8..2000){
+                        url = "https://loremflickr.com/$w/$h/brazil"
+                    }else{
+                        error = "Please enter width and height again."
+                    }
+                }else{
+                    error = "Please enter width and height."
+                }}) {
+                Text(
+                    text = "brazil",
+                    fontSize = 20.sp,
+                )
+            }
+        }
 
-        Button(onClick = { /*TODO*/ }) {
+        Text(text = error)
+        if (url != ""){
+            Image(
+                painter = rememberImagePainter(url),
+                contentDescription = "Random Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+            )
+        }
+        Button(onClick = {
+            error = ""
+            url = ""
+            width = ""
+            height = ""
+        }) {
             Text(
-                text = "Submit",
+                text = "Reset",
                 fontSize = 20.sp,
             )
         }
-    }
-}
 
-@Composable
-fun ShowPic(name: String, navController: NavController, modifier: Modifier = Modifier){
-    Column() {
+
 
     }
 }
@@ -124,8 +369,5 @@ fun ShowPic(name: String, navController: NavController, modifier: Modifier = Mod
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Random_ImageTheme {
-        val navController = rememberNavController()
-        HomeScreen(navController, modifier = Modifier)
-    }
+    Random_ImageTheme {}
 }
